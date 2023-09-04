fb56af72028c33c3d04aeaebd8d8bedf52d1da93
Mahathi1999 <124342355+Mahathi1999@users.noreply.github.com>
2023-09-04 16:45:32 +0530

    Add files via upload

---
+++ client/build/precache-manifest.6a27fcb66f4ffd50721199fa73182ca4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b85804bba0392d9fcacef66708d8337",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae758ada30f0a0d95195",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "94722200f2bb2c54f87b",
+    "url": "/static/js/2.32d91e32.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.32d91e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae758ada30f0a0d95195",
+    "url": "/static/js/main.2c86b9aa.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
